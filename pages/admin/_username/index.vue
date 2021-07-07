dfe0ba86bf9750e9fe4cd951091a57393ca64664
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-07-07 23:16:12 +0700

    first commit

---
+++ pages/admin/_username/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Username Admin : {{$route.params.username}} </div>
+</template>
+
+<script>
+export default {
+    validate(data) {
+        return /^\d+$/.test(data.params.username);
+    }
+}
+</script>
+
+<style>
+
+</style>
