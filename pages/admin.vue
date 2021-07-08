eb5febbd1b563037b9bc551a9d21b17161ee1181
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-07-08 23:32:43 +0700

    admin

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
+    <div>
       <h2>Form User</h2>
       <form action="">
-          <input type="text" name="" id="">
-          <input type="submit" name="" id="">
+        <input type="text" name="" id="" />
+        <input type="submit" name="" id="" />
       </form>
-      <nuxt-child />
+       <nuxt-child />
+    </div>
+
+   
   </div>
 </template>
 
 <script>
 export default {
-    layout: "coreLayout"
+     layout: 'coreLayout',
+
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
