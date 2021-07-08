6f388b06bd578e950b42f62f0dfcbc7fa407bec2
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-07-08 23:15:24 +0700

    layout

---
+++ pages/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h2>Form User</h2>
+      <form action="">
+          <input type="text" name="" id="">
+          <input type="submit" name="" id="">
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      My post 
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
