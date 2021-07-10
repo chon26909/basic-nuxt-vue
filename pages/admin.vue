d7875210994cf2ad8b73d45784a673ea1a062111
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-07-10 21:12:28 +0700

    carousel

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
-    <div>
+    <div class="card">
       <h2>Form User</h2>
       <form action="">
         <input type="text" name="" id="" />
-        <input type="submit" name="" id="" />
+        <input type="submit" class="btn btn-primary" name="" id="" />
       </form>
        <nuxt-child />
     </div>
-
-   
   </div>
 </template>
 
 <script>
 export default {
-     layout: 'coreLayout',
-
+  layout: 'admin'
 }
 </script>
 
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-      My post 
+    <b-container>
+      <h3>My Post Content</h3>
+      <Header></Header>
+      <PostContent></PostContent>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default {
+import Header from '@/components/posts/Header.vue'
+import PostContent from '@/components/posts/PostContent.vue'
 
+export default {
+  components: {
+    Header,
+    PostContent,
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
